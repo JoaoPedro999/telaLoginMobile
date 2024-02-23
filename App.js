c5fde3ad9af0e2aba97f79ca899462af825d4b6d
João Pedro Araújo Teixeira <116459774+JoaoPedro999@users.noreply.github.com>
2024-02-23 10:19:38 -0300

    loginmobile

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Image, ImageBackground } from 'react-native';
 
 export default function App() {
+
+  btn = () => {
+    alert("Logado com Sucesso");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+       <ImageBackground source={require('./assets/fundo.jpg')} resizeMode="cover" style={styles.image}>
+      <Text style={styles.titulo}>Login</Text>
+      
+      <TextInput style={styles.inputname} placeholder='Digite seu Nome' />
+      <TextInput style={styles.inputname} placeholder='Digite seu E-mail' />
+      <TextInput style={styles.inputname} placeholder='Digite sua Senha' secureTextEntry={true}/>
+      <Button 
+        onPress={btn}
+        style={styles.button} 
+        title='Logar'  
+      />
+      <Image
+        style={styles.senailogo}
+        source={require('./assets/SENAI.png')}
+      />
+        </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +33,46 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'white',
+    fontSize: 50,
+  },
+  inputname: {
+    color: 'black',
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 10,
+    width: 300,
+    height: 50, 
+    backgroundColor: 'white',
+  
+  },
+  button: {
+    color: 'white',
+    backgroundColor: 'black',
+    fontSize: 20,
+  },
+  titulo: {
+    fontWeight: 'bold',
+    fontSize: 50,
+    marginBottom: 20,
+    borderBottomColor: 'black',
+    borderBottomWidth: 2,
+  },
+  senailogo: {
+   width: 250,
+   height: 50,
+   bottom: 400,
+  },
+  image: {
+    justifyContent: 'center',
+    width:'100%',
+    height: '100%',
+  },
 });
+
+
+

9cf64ba51b02c84d7f3b46aafbb3c4dfbc9a6bcb
João Pedro Araújo Teixeira <116459774+JoaoPedro999@users.noreply.github.com>
2024-02-28 07:51:26 -0300

    commitprettier

--- App.js
+++ App.js
@@ -1,33 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button, Image, ImageBackground } from 'react-native';
-import SecondComponent from './components/secondcomponents';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  Image,
+  ImageBackground,
+} from "react-native";
+import SecondComponent from "./components/secondcomponents";
 
 export default function App() {
-
   btn = () => {
     alert("Logado com Sucesso");
-  }
+  };
 
   return (
     <View style={styles.container}>
-      <ImageBackground source={require('./assets/fundo.jpg')} resizeMode="cover" style={styles.image}>
+      <ImageBackground
+        source={require("./assets/fundo.jpg")}
+        resizeMode="cover"
+        style={styles.image}
+      >
         <Text style={styles.titulo}>Login</Text>
 
-        <TextInput style={styles.inputname} placeholder='Digite seu Nome' />
-        <TextInput style={styles.inputname} placeholder='Digite seu E-mail' />
-        <TextInput style={styles.inputname} placeholder='Digite sua Senha' secureTextEntry={true} />
-        <Button 
-          onPress={btn}
-          style={styles.button}
-          title='Logar'
+        <TextInput style={styles.inputname} placeholder="Digite seu Nome" />
+        <TextInput style={styles.inputname} placeholder="Digite seu E-mail" />
+        <TextInput
+          style={styles.inputname}
+          placeholder="Digite sua Senha"
+          secureTextEntry={true}
         />
+        <Button onPress={btn} style={styles.button} title="Logar" />
         <Image
           style={styles.senailogo}
-          source={require('./assets/SENAI.png')}
+          source={require("./assets/SENAI.png")}
         />
 
         <StatusBar style="auto" />
-      <SecondComponent/>
+        <SecondComponent />
       </ImageBackground>
     </View>
   );
@@ -38,32 +49,31 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   text: {
-    color: 'white',
+    color: "white",
     fontSize: 50,
   },
   inputname: {
-    color: 'black',
+    color: "black",
     borderWidth: 1,
-    borderColor: 'lightgray',
+    borderColor: "lightgray",
     marginBottom: 10,
     width: 300,
     height: 50,
-    backgroundColor: 'lightgray',
-    opacity: '80%',
-
+    backgroundColor: "lightgray",
+    opacity: "80%",
   },
   button: {
-    color: 'white',
-    backgroundColor: 'black',
+    color: "white",
+    backgroundColor: "black",
     fontSize: 20,
-    width: '50',
-    height: '25',
+    width: "50",
+    height: "25",
   },
   titulo: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 50,
     marginBottom: 20,
-    borderBottomColor: 'black',
+    borderBottomColor: "black",
     borderBottomWidth: 2,
   },
   senailogo: {
@@ -73,11 +83,8 @@ const styles = StyleSheet.create({
   },
   image: {
     flex: 1,
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
-
-
-
